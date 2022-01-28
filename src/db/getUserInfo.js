2453be93bd9baf99188f035e216707e4297c7323
Dmitrii Kilishek <mentalclear@gmail.com>
2022-01-28 07:46:57 -0500

    :tada: Done!

--- src/db/getUserInfo.js
+++ src/db/getUserInfo.js
@@ -2,8 +2,8 @@ import { getUser } from './getUser';
 import { getCurrentSharePrice } from './getCurrentSharePrice';
 
 
-export const getUserInfo = async () => {
-    const currentSharePrice = await getCurrentSharePrice();
+export const getUserInfo = async (stockHistory) => {
+    const currentSharePrice = await getCurrentSharePrice(stockHistory);
     const  { cashValue, numberOfSharesOwned } = await getUser();
     const sharesValue = currentSharePrice * numberOfSharesOwned;
     return {
--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,6 @@
 export { db } from './db';
+export { buyStock } from './buyStock';
 export { getUserInfo } from './getUserInfo';
 export { getUser } from './getUser';
 export { getCurrentSharePrice } from './getCurrentSharePrice';
+export { sellStock } from './sellStock';
